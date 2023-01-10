5bb35c9200295817c255aaf25c12b07ff64394c1
mdashikulislam <ashik.nwu@gmail.com>
2023-01-10 23:04:04 +0600

    update profile block

--- profile.php
+++ profile.php
@@ -1086,9 +1086,11 @@ flux_hook('profile_after_form_handling');
 
 
 // мод пола - add "g.g_pm, u.messages_enable," - New PMS - Visman
-$result = $db->query('SELECT u.username, u.gender, u.email, u.title, u.realname, u.url, u.jabber, u.icq, u.location, u.signature, u.disp_topics, u.disp_posts, u.email_setting, u.notify_with_post, u.auto_notify, u.show_smilies, u.show_img, u.show_img_sig, u.show_avatars, u.show_sig, u.timezone, u.dst, u.language, u.style, u.num_posts, u.last_post, u.registered, u.registration_ip, u.admin_note, u.date_format, u.time_format, u.last_visit, u.messages_enable, g.g_id, g.g_user_title, g.g_moderator, g.g_pm FROM '.$db->prefix.'users AS u LEFT JOIN '.$db->prefix.'groups AS g ON g.g_id=u.group_id WHERE u.id='.$id) or error('Unable to fetch user info', __FILE__, __LINE__, $db->error());
+$result = $db->query('SELECT u.id,u.username, u.gender, u.email, u.title, u.realname, u.url, u.jabber, u.icq, u.location, u.signature, u.disp_topics, u.disp_posts, u.email_setting, u.notify_with_post, u.auto_notify, u.show_smilies, u.show_img, u.show_img_sig, u.show_avatars, u.show_sig, u.timezone, u.dst, u.language, u.style, u.num_posts, u.last_post, u.registered, u.registration_ip, u.admin_note, u.date_format, u.time_format, u.last_visit, u.messages_enable, g.g_id, g.g_user_title, g.g_moderator, g.g_pm FROM '.$db->prefix.'users AS u LEFT JOIN '.$db->prefix.'groups AS g ON g.g_id=u.group_id WHERE u.id='.$id) or error('Unable to fetch user info', __FILE__, __LINE__, $db->error());
 $user = $db->fetch_assoc($result);
-
+if ($user['id'] != $pun_user['id']){
+    message($lang_common['Bad request'], false, '404 Not Found');
+}
 if (!$user)
 	message($lang_common['Bad request'], false, '404 Not Found');
 
